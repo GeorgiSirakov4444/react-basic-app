7645d5ece9f397eee7d440b67b1c37c971a1cfea
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-07 14:39:00 +0200

    Fragments, Portals and refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,18 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from "../UI/ErrorModal";
 import Success from "../UI/Success";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
-    const [enteredEmail, setEnteredEmail] = useState('');
     const [error, setError] = useState();
     const [success, setSuccess] = useState();
+
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+    const emailInputRef = useRef();
 
     function isValidEmail(email) {
         return /\S+@\S+\.\S+/.test(email);
@@ -18,21 +20,25 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || enteredEmail.trim().length === 0) {
+        const enteredName = nameInputRef.current.value;
+        const enteredAges = ageInputRef.current.value;
+        const enteredEmails = emailInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredEmails.trim().length === 0) {
             setError({
                 title: "Error occured!",
                 message: "Please input correct data!"
             });
             return;
         }
-        if (!isValidEmail(enteredEmail)) {
+        if (!isValidEmail(enteredEmails)) {
             setError({
                 title: "Error !",
                 message: "Invalid Email!"
             });
             return;
         }
-        if (+enteredAge < 1) {
+        if (+enteredAges < 1) {
             setError({
                 title: "ERR OR!",
                 message: "Age is smaller than 1 !"
@@ -43,21 +49,13 @@ const AddUser = (props) => {
             title: "Success!",
             message: "You successfully registered a User!"
         });
-        props.onAddUser(enteredUsername, enteredAge, enteredEmail);
-        setEnteredUsername('');
-        setEnteredAge('');
-        setEnteredEmail('');
+        props.onAddUser(enteredName, enteredAges, enteredEmails);
+
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+        emailInputRef.current.value = '';
     };
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    };
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    };
-    const emailChangeHandler = (event) => {
-        setEnteredEmail(event.target.value);
-    };
     const errorHandler = () => {
         setError(null);
     };
@@ -66,23 +64,23 @@ const AddUser = (props) => {
     };
 
     return (
-        <div>
-        {error && <ErrorModal title={error.title} message={error.message} error={errorHandler}/>}
+        <Wrapper>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
         {success && <Success title={success.title} message={success.message} success={successHandler}/>} 
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler}></input>
+            <input type="text" id="username" ref={nameInputRef}></input>
             <label htmlFor="age">Age(years)</label>
-            <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}></input>
+            <input type="number" id="age" ref={ageInputRef}></input>
             <label htmlFor="email">Email</label>
-            <input type='text' id="email" value={enteredEmail} onChange={emailChangeHandler}></input>
+            <input type='text' id="email" ref={emailInputRef}></input>
             <Button  
                 type="submit">Create User
             </Button>
         </form>
         </Card>
-        </div>
+        </Wrapper>
     );
 };
 
